b378a8c9211ba046f1b6461880674f98440b187a
CorpTechFist <devcorptechfist@gmail.com>
2025-10-14 19:18:10 +0530

    ./

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/', // add this line
+    base: './', // add this line
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
