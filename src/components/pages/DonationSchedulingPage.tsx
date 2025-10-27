1e0b1820c2b2e82402526e1eaeccff7680dde297
CorpTechFist <devcorptechfist@gmail.com>
2025-10-27 18:34:20 +0530

    Update DonationSchedulingPage.tsx

--- src/components/pages/DonationSchedulingPage.tsx
+++ src/components/pages/DonationSchedulingPage.tsx
@@ -620,7 +620,7 @@ export function DonationSchedulingPage() {
                 <div className="w-10 h-10 rounded-full bg-purple-100 flex items-center justify-center">
                   <Mail className="w-5 h-5 text-purple-600" />
                 </div>
-                <span className="text-gray-800" style={{ fontWeight: '500' }}>donations@wamalliance.org</span>
+                <span className="text-gray-800" style={{ fontWeight: '500' }}>weallmatter@wamalliance.org</span>
               </div>
             </div>
           </CardContent>
